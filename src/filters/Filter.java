2d7384a6a5c47c08c23dd9535f03eaf21d56febf
EduardoHuerta <56246959+EduardoHuerta@users.noreply.github.com>
2021-03-05 20:46:05 -0700

    Se agregaron hilos

--- src/filters/Filter.java
+++ src/filters/Filter.java
@@ -1,25 +1,26 @@
 package filters;
 
-public abstract class Filter implements Runnable {
+public abstract class Filter implements Runnable{
 
     protected Pipe input,output;   //Declaramos una tuberia para entrada
                                    //Declaramos una tuberia para salida
+
+    long initialTime = System.currentTimeMillis();
 
     public Filter(Pipe in, Pipe out){
         input = in;
         output = out;
     }
-
     public void start() {
         Thread thread = new Thread(this);
-        System.out.println("Hilo:" +thread.getName() + "iniciando");
         thread.start();
+        System.out.println("EJECUTANDO.... "+ thread.currentThread().getName());
     }
 
     @Override
     public void run() {
         transform();
+        System.out.println((System.currentTimeMillis() - initialTime)	+ "milisegs" );
     }
-
     public abstract void transform();
 }
